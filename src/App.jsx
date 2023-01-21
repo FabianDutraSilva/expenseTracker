5494174e00ac0e8fa954672e9354087ef39c8448
Fabian Dutra Silva <fabiandutrasilva@gmail.com>
2023-01-21 09:56:36 -0300

    ExpTracker: add NewExpense form component and pass data from child to parent, add change tittle dummy button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -22,12 +23,18 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expenses);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses item={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses item={expenses} />
     </div>
   );
-}
+};
 
 export default App;
